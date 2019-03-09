6cd740eeba1ea8b93aa4cfdebc35533cf2e1f558
Aleksander Serdukov <serdukovaa@yandex.ru>
2019-03-09 18:06:22 +0300

    Настроены скрипты gradle. Медленно но работает

--- render/src/commonMain/kotlin/com/nixvision/support/terminal/render/components/HeadComponent.kt
+++ render/src/commonMain/kotlin/com/nixvision/support/terminal/render/components/HeadComponent.kt
@@ -2,10 +2,17 @@ package com.nixvision.support.terminal.render.components
 
 import kotlinx.html.HTML
 import kotlinx.html.head
+import kotlinx.html.script
 
 
 fun HTML.mainHead() {
     head{
+        script{
+            src = "/lib.js"
+        }
+        script{
+            src = "/script.js"
+        }
         generalStyle()
     }
 }

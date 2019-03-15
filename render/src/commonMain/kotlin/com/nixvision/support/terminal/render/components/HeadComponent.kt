21a3dedf4ad61fb267c22dcf3ee2e05a37ffad3e
Aleksander Serdukov <serdukovaa@yandex.ru>
2019-03-15 17:08:29 +0300

    Тег мета не закрытый, поэтому не валидный

--- render/src/commonMain/kotlin/com/nixvision/support/terminal/render/components/HeadComponent.kt
+++ render/src/commonMain/kotlin/com/nixvision/support/terminal/render/components/HeadComponent.kt
@@ -1,15 +1,12 @@
 package com.nixvision.support.terminal.render.components
 
-import kotlinx.html.HTML
-import kotlinx.html.head
-import kotlinx.html.meta
-import kotlinx.html.script
+import kotlinx.html.*
 
 
 fun HTML.mainHead() {
     head {
-        meta {
-            charset = "utf-8"
+        unsafe {
+            raw("""<meta charset="UTF-8"/>""")
         }
         script {
             src = "/main.bundle.js"
